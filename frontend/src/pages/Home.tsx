8a12f7c2ace8b7b9e359c95909d954a66599e842
debe7408 <bendaravicius.deividas@gmail.com>
2023-03-19 04:16:17 +0200

    feat: added simple api call to backend

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,29 @@
+import { Button } from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
+
 const Home = () => {
-  return <p>Home page</p>;
+  const [responseData, setResponseData] = useState(null);
+
+  const handleOnClick = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/api");
+      setResponseData(response.data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <h1>Home</h1>
+      <Button variant="contained" onClick={handleOnClick}>
+        {" "}
+        Hello World
+      </Button>
+      {responseData && <p>{responseData}</p>}
+    </>
+  );
 };
 
 export default Home;
