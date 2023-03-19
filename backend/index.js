e33d889276ff140cb1f4f30fc9f549c40b44f66e
debe7408 <bendaravicius.deividas@gmail.com>
2023-03-19 04:16:05 +0200

    feat: added simple api route

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,12 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server listening at port ${process.env.PORT}`)
-);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -16,3 +15,11 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.send("About route");
 });
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server listening at port ${process.env.PORT}`)
+);
