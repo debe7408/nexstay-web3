62bc58a648cdbf7b9a6af450b2ea49dd9a7d7cb4
debe7408 <bendaravicius.deividas@gmail.com>
2023-03-20 17:27:24 +0200

    chore: update routers FE

--- frontend/src/components/RouteComponent.tsx
+++ frontend/src/components/RouteComponent.tsx
@@ -1,19 +1,19 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-
 import Home from "../pages/Home";
 import RegisterPage from "../pages/RegisterPage";
-import { SignIn } from "../pages/SignInPage";
+import SigninPage from "../pages/SignInPage";
+
 const Main = () => {
   const location = useLocation();
 
   return (
-    <TransitionGroup>
+    <TransitionGroup component={null}>
       <CSSTransition key={location.key} classNames="fade" timeout={300}>
-        <Routes>
+        <Routes location={location}>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signin" element={<SigninPage />} />
         </Routes>
       </CSSTransition>
     </TransitionGroup>
