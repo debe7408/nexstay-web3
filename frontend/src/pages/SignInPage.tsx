8d2d6bfc14000acb6aec0807f800e5f984cf1292
debe7408 <bendaravicius.deividas@gmail.com>
2023-03-20 17:28:48 +0200

    feat: login form

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -1,106 +1,18 @@
-import {
-  Container,
-  CssBaseline,
-  Box,
-  Avatar,
-  Typography,
-  TextField,
-  FormControlLabel,
-  Checkbox,
-  Button,
-  Grid,
-} from "@mui/material";
-import { Link } from "react-router-dom";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import { useForm, SubmitHandler } from "react-hook-form";
+import React from "react";
+import CustomCard from "../components/CustomCard";
+import LoginForm from "../components/LoginForm";
 
-type Inputs = {
-  email: string;
-  password: string;
-};
-
-export const SignIn = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<Inputs>();
-
-  const onSubmit: SubmitHandler<Inputs> = (formValues, event) => {
-    event?.preventDefault();
-    console.log(formValues);
-    alert("Form submitted!");
-  };
-
-  const onError = (error: any) => console.log(error);
-
-  console.log(watch("email"));
-
+const SigninPage = () => {
   return (
-    <Container component="main" maxWidth="xs">
-      <form onSubmit={handleSubmit(onSubmit, onError)}>
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            autoComplete="email"
-            autoFocus
-            {...register("email", { required: true })}
-          />
-          {errors.email && <span>Invalid email address</span>}
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            {...register("password", { required: true })}
-          />
-          {errors.password && <span>This field is required</span>}
-
-          <FormControlLabel
-            control={<Checkbox value="true" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link to={""}>Forgot password?</Link>
-            </Grid>
-            <Grid item>
-              <Link to={""}>Don't have an account? Sign Up</Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </form>
-    </Container>
+    <>
+      <CustomCard
+        title="Login"
+        subtitle="Welcome back! Please login to your account."
+        body={<LoginForm />}
+        cardWidth="md"
+      />
+    </>
   );
 };
+
+export default SigninPage;
