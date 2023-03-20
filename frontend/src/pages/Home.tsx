f8cd55200c30ffbb91482555bb0f353437f6bb22
debe7408 <bendaravicius.deividas@gmail.com>
2023-03-20 02:38:25 +0200

    test: simple backend test

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,17 +1,54 @@
-import { Button } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
 import axios from "axios";
 import React, { useState } from "react";
 
 const Home = () => {
-  const [responseData, setResponseData] = useState(null);
+  const [responseData, setResponseData] = useState<User>();
 
+  type User = [
+    {
+      id: number;
+      name: string;
+      email: string;
+      age: number;
+      banned: boolean;
+      type: string;
+      surname: string;
+      password: string;
+    }
+  ];
   const handleOnClick = async () => {
     try {
-      const response = await axios.get("http://localhost:3001/api");
-      setResponseData(response.data.message);
+      const response = await axios.get("http://localhost:3001/api/users");
+      console.log(response);
+      setResponseData(response.data);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const CustomDivBody = (data: User) => {
+    return (
+      <Grid container>
+        {data.map((user) => (
+          <Box
+            key={user.id}
+            sx={{
+              border: 1,
+              margin: 1,
+            }}
+          >
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            <p>{user.age}</p>
+            <p>{user.banned}</p>
+            <p>{user.type}</p>
+            <p>{user.surname}</p>
+            <p>{user.password}</p>
+          </Box>
+        ))}
+      </Grid>
+    );
   };
 
   return (
@@ -21,7 +58,7 @@ const Home = () => {
         {" "}
         Hello World
       </Button>
-      {responseData && <p>{responseData}</p>}
+      {responseData && CustomDivBody(responseData)}
     </>
   );
 };
