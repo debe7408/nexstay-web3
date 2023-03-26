b3966915255dfbe7f8c26c745a6c65e6a6fd5264
debe7408 <bendaravicius.deividas@gmail.com>
2023-03-27 00:09:03 +0300

    experimental: bottom nav for mobile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,25 @@
 import Navbar from "./components/Navbar";
 import { initAxiosClient } from "./axios/axiosClient";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Routes from "./components/RouteComponent";
 import { useAppSelector } from "./app/hooks";
 import { selectAuthToken } from "./app/loginSlice";
+import { useMediaQuery } from "@mui/material";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreIcon from "@mui/icons-material/Explore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import styled from "styled-components";
 
 export default function App() {
   const authorizationToken = useAppSelector(selectAuthToken);
+  const matches = useMediaQuery("(min-width:600px)");
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
 
   useEffect(() => {
     initAxiosClient(authorizationToken);
@@ -16,6 +29,19 @@ export default function App() {
     <>
       <Navbar></Navbar>
       <Routes></Routes>
+      {!matches && (
+        <StyledBottomNavigation value={value} onChange={handleChange}>
+          <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+          <BottomNavigationAction label="Explore" icon={<ExploreIcon />} />
+          <BottomNavigationAction label="Profle" icon={<FavoriteIcon />} />
+        </StyledBottomNavigation>
+      )}
     </>
   );
 }
+
+const StyledBottomNavigation = styled(BottomNavigation)`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`;
