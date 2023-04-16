8da62653e9dbf62b37f801fd73b45c49cbb8c0c6
debe7408 <bendaravicius.deividas@gmail.com>
2023-04-16 16:21:31 +0300

    feat: mockup of web3auth login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,43 @@
 import Navbar from "./components/Navbar";
 import { initAxiosClient } from "./axios/axiosClient";
-import { useEffect, useState } from "react";
+import {
+  connectWeb3auth,
+  disconnectWeb3auth,
+  initWeb3Auth,
+} from "./web3/web3auth";
+import { useEffect } from "react";
 import Routes from "./components/RouteComponent";
-import { useAppSelector } from "./app/hooks";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
 import { selectAuthToken } from "./app/loginSlice";
-import { useMediaQuery } from "@mui/material";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import HomeIcon from "@mui/icons-material/Home";
-import ExploreIcon from "@mui/icons-material/Explore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import styled from "styled-components";
+import { setWeb3AuthState, web3authSelector } from "./app/web3Slice";
+import { Button } from "@mui/material";
 
 export default function App() {
+  const dispatch = useAppDispatch();
   const authorizationToken = useAppSelector(selectAuthToken);
-  const matches = useMediaQuery("(min-width:600px)");
-  const [value, setValue] = useState(0);
-
-  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-    setValue(newValue);
-  };
+  const web3AuthInstance = useAppSelector(web3authSelector);
 
   useEffect(() => {
+    const init = async () => {
+      const web3auth = await initWeb3Auth();
+      dispatch(setWeb3AuthState(web3auth));
+    };
+
     initAxiosClient(authorizationToken);
+
+    init();
   }, [authorizationToken]);
 
   return (
     <>
       <Navbar></Navbar>
       <Routes></Routes>
-      {!matches && (
-        <StyledBottomNavigation value={value} onChange={handleChange}>
-          <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-          <BottomNavigationAction label="Explore" icon={<ExploreIcon />} />
-          <BottomNavigationAction label="Profle" icon={<FavoriteIcon />} />
-        </StyledBottomNavigation>
-      )}
+      <Button onClick={async () => await connectWeb3auth(web3AuthInstance)}>
+        OPEN MODAL
+      </Button>
+      <Button onClick={() => disconnectWeb3auth(web3AuthInstance)}>
+        LOGOUT
+      </Button>
     </>
   );
 }
-
-const StyledBottomNavigation = styled(BottomNavigation)`
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-`;
