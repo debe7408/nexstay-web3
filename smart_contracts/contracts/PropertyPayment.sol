27dbb45902fcc889b1c19523adfdc5d8bfc63e61
debe7408 <bendaravicius.deividas@gmail.com>
2023-05-13 14:27:05 +0300

    feat: update payment transaction

--- smart_contracts/contracts/PropertyPayment.sol
+++ smart_contracts/contracts/PropertyPayment.sol
@@ -6,23 +6,36 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract PropertyPayment {
     address public owner;
     IERC20 public token;
+    uint256 public feePercentage;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Caller is not the owner");
         _;
     }
 
-    event PaymentReceived(address indexed payer, uint256 amount);
+    event PaymentReceived(
+        address indexed payer,
+        address receiver,
+        uint256 amount
+    );
 
-    constructor(address _token) public {
+    constructor(address _token, uint256 _feePercentage) {
+        require(_feePercentage <= 100, "Invalid fee percentage");
         owner = msg.sender;
         token = IERC20(_token);
+        feePercentage = _feePercentage;
     }
 
-    function pay(uint256 amount) external {
+    function pay(uint256 amount, address propertyOwner) external {
         require(amount > 0, "Payment amount must be greater than zero.");
-        token.transferFrom(msg.sender, owner, amount);
-        emit PaymentReceived(msg.sender, amount);
+
+        uint256 feeAmount = (amount * feePercentage) / 100;
+        uint256 propertyOwnerAmount = amount - feeAmount;
+
+        token.transferFrom(msg.sender, owner, feeAmount);
+        token.transferFrom(msg.sender, propertyOwner, propertyOwnerAmount);
+
+        emit PaymentReceived(msg.sender, propertyOwner, amount);
     }
 
     function transferOwnership(address _newOwner) external onlyOwner {
@@ -33,5 +46,10 @@ contract PropertyPayment {
     function setTokenAddress(address _newToken) external onlyOwner {
         require(_newToken != address(0), "Invalid Token address");
         token = IERC20(_newToken);
+    }
+
+    function setFeePercentage(uint256 _newFeePercentage) external onlyOwner {
+        require(_newFeePercentage <= 100, "Invalid fee percentage");
+        feePercentage = _newFeePercentage;
     }
 }
--- smart_contracts/migrations/2_deploy_property_payment.js
+++ smart_contracts/migrations/2_deploy_property_payment.js
@@ -2,11 +2,15 @@ const PropertyPayment = artifacts.require("PropertyPayment");
 
 module.exports = async function (deployer, network) {
   // Token address that is expected for payment transaction
-  // Defaults to USDC on BSC testnet, can be changed by owner of the contract calling setTokenAddress()
+  // Defaults to USDT on BSC testnet, can be changed by owner of the contract calling setTokenAddress()
   const token = "0x6fc24c93a5665845f7455eb1a72fb1f47510af42";
 
+  // Fee percentage that is expected for payment transaction
+  // Defaults to 5%, can be changed by owner of the contract calling setFeePercentage()
+  const feePercentage = "5";
+
   if (network === "bsctestnet") {
-    await deployer.deploy(PropertyPayment, token);
+    await deployer.deploy(PropertyPayment, token, feePercentage);
   } else {
     // Deploy a Mock Token for the local development network
     const MockToken = artifacts.require("MockToken");
