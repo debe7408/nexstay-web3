71f25f1626b64596b2ef19c3b750acd9661557fa
debe7408 <bendaravicius.deividas@gmail.com>
2023-05-16 01:25:46 +0300

    feat: updated transaction function

--- frontend/src/web3/transactions.ts
+++ frontend/src/web3/transactions.ts
@@ -1,19 +1,25 @@
-import { ethers } from "ethers";
+import { ethers, ContractTransaction, ContractReceipt } from "ethers";
 import type { Web3Provider } from "@ethersproject/providers";
 import PropertyPaymentABI from "./abi/PropertyPaymentABI.json";
 import ERC20ABI from "./abi/ERC20ABI.json";
+
+const contractAddress = process.env
+  .REACT_APP_PAYMENT_CONTRACT_ADDRESS as string;
+
+const tokenAddress = process.env.REACT_APP_USDT_TOKEN_ADDRESS as string;
+
+interface TransactionResponse {
+  txReceipt: ContractReceipt | undefined;
+  error?: string;
+}
 
 export const paymentTransaction = async (
   provider: Web3Provider,
   amount: string | number,
   receiver: string
-) => {
+): Promise<TransactionResponse> => {
   try {
     const signer = provider.getSigner();
-
-    const contractAddress = process.env
-      .REACT_APP_PAYMENT_CONTRACT_ADDRESS as string;
-    const tokenAddress = process.env.REACT_APP_USDT_TOKEN_ADDRESS as string;
 
     const weiAmount = ethers.utils.parseUnits(amount.toString(), "ether");
 
@@ -23,19 +29,47 @@ export const paymentTransaction = async (
       signer
     );
 
-    const tokenContract = new ethers.Contract(tokenAddress, ERC20ABI, signer);
-
-    const approveTxResponse = await tokenContract.approve(
-      tokenAddress,
-      weiAmount
+    const txResponse: ContractTransaction = await contract.pay(
+      weiAmount,
+      receiver
     );
-    const approveTxReceipt = await approveTxResponse.wait();
 
-    const txResponse = await contract.pay(weiAmount, receiver);
     const txReceipt = await txResponse.wait();
 
-    return txReceipt;
+    return { txReceipt };
   } catch (error) {
-    console.log(error);
+    return {
+      txReceipt: undefined,
+      error:
+        "Payment transaction failed. Either you rejected the transaction or the approved amount is not the same.",
+    };
+  }
+};
+// TODO IMPROVEMENT: Add a check to see if the user has already approved the contract to spend their tokens
+export const approvalTransaction = async (
+  provider: Web3Provider,
+  amount: string | number
+): Promise<TransactionResponse> => {
+  try {
+    const signer = provider.getSigner();
+    const signerAddress = await signer.getAddress();
+
+    const weiAmount = ethers.utils.parseUnits(amount.toString(), "ether");
+
+    const tokenContract = new ethers.Contract(tokenAddress, ERC20ABI, signer);
+
+    const txResponse: ContractTransaction = await tokenContract.approve(
+      contractAddress,
+      weiAmount
+    );
+    const txReceipt = await txResponse.wait();
+
+    return { txReceipt };
+  } catch (error) {
+    return {
+      txReceipt: undefined,
+      error:
+        "Approval failed. Either you rejected the transaction or you don't have enough balance",
+    };
   }
 };
